b8ed6e3b0de5f697f4fdbc165486390de5c73eeb
Jose <jloai009@fiu.edu>
2021-11-05 10:00:51 -0400

    Added ValidationError handling

--- NotesApp backend/index.js
+++ NotesApp backend/index.js
@@ -96,7 +96,7 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -111,10 +111,13 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote)
+    })
+    .catch(error => next(error))
 
 })
 
@@ -128,6 +131,8 @@ const errorHandler = (error, request, reponse, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformed id' })
+  } else if (error.name === 'ValidationError') {
+    return reponse.status(400).json({ error: error.message })
   }
 
   next(error)
