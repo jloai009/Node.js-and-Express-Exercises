89504eb2859a50422151381fd1562fed99639949
Jose <jloai009@fiu.edu>
2021-11-06 08:06:13 -0400

    Add GET handlers

---
+++ NotesApp backend/controllers/notes.js
@@ -0,0 +1,20 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+notesRouter.get('/', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+notesRouter.get('/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
