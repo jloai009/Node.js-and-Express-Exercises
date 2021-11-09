19d739cfa9dd22b156e72d565914af9844f7dc16
Jose <jloai009@fiu.edu>
2021-11-09 03:29:58 -0500

    Add GET routing

--- controllers/users.js
+++ controllers/users.js
@@ -13,10 +13,13 @@ usersRouter.post('/', async (request, response) => {
     name: body.name,
     passwordHash,
   })
-
   const savedUser = await user.save()
-
   response.json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
 })
 
 module.exports = usersRouter
